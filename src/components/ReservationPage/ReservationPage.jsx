c6255ac7f48a6808e2958f9aeb882c7a3fd3a82b
Sára Hölclová <sara.holclova@gmail.com>
2024-05-18 10:04:24 +0200

    nastavení routování a komponenta ReservationPage

---
+++ src/components/ReservationPage/ReservationPage.jsx
@@ -0,0 +1,3 @@
+export const ReservationPage = () => {
+  return <h2>Vaše jízdenka</h2>;
+};
--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,26 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 import { App } from './components/App';
 import './global.css';
+import { HomePage } from './pages/HomePage';
+import { ReservationPage } from './components/ReservationPage/ReservationPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+    ],
+  },
+  {
+    path: '/reservation',
+    element: <ReservationPage />,
   },
 ]);
 
 createRoot(document.querySelector('#app')).render(
-  <RouterProvider router={router} />
+  <RouterProvider router={router} />,
 );
